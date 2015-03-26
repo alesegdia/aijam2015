2094dcdf0b09b08b58ced46dd7b8f8933e670835
Alejandro Seguí <alesegdia@gmail.com>
2015-03-26 03:03:42 +0100

    Sketching boids a little more.

--- project/src/ai/GlobalMind.lua
+++ project/src/ai/GlobalMind.lua
@@ -8,7 +8,9 @@ GlobalMind = Class {
 
 	minions = {},
 
-	init = function(self, hero, zombies)
+	init = function(self, hero, zombies, teamid)
+		self.teamid = teamid
+		-- spawn individuals
 		self.minions = {}
 		for k,v in pairs(zombies) do
 			self.minions[k] = IndividualMind(self,v)
@@ -17,18 +19,30 @@ GlobalMind = Class {
 
 	step = function(self)
 		-- Vectores de influencia:
-		-- * Separación (gira para evitar vecinos locales)
-		-- * Alineación (giro hacia posición media)
-		-- * 
+		-- * Separación (gira para evitar obstáculos y vecinos locales)
+		-- * Alineación (apuntar hacia donde apunta la media)
+		-- * Cohesión (giro hacia la posición media)
+		-- * Objetivo Hero? mayble laterakfakjsdfjasjdjjfjsdjfa
+		-- ASSUMING CONTROL
+		-- Iterate twice:
+		-- 1. Reset steers, sum positions, perform perception
+		-- 2. With sum, get centroid and calculate final steering, then apply steering
 		local mid = Vector(0,0)
 		for k, v in pairs(self.minions) do
+
+			v:resetSteer()
+
 			mid.x = mid.x + v.zombie.pos.x
 			mid.y = mid.y + v.zombie.pos.y
+
+			v:perceive()
+
 		end
 		mid.x = mid.x / #self.minions
 		mid.y = mid.y / #self.minions
+
 		for k, v in pairs(self.minions) do
-			v:step()
+			v:applySteer()
 		end
 	end
 
--- project/src/ai/IndividualMind.lua
+++ project/src/ai/IndividualMind.lua
@@ -9,10 +9,55 @@ IndividualMind = Class {
 		self.globalmind = globalmind
 		self.pawn = pawn
 		self.forward = Vector(0,0)
+		self.thurst = 0
+		self.steer = 0
+		self.influenceWeight = {
+			separation = 1,
+			alignment = 1,
+			cohesion = 1,
+			objective = 1
+		}
+		self.influenceVecs = {
+			separation = Vector(0,0),
+			alignment = Vector(0,0),
+			cohesion = Vector(0,0),
+			objective = Vector(0,0)
+		}
 	end,
 
-	step = function(self)
+	resetSteer = function(self)
+		self.steer = 0
+	end,
 
+	applySteer = function(self)
+		self.pawn.physicbody.setAngle(self.pawn.physicbody:getAngle() + self.steer)
+	end,
+
+	perceive = function(self)
+		self.perceived = {}
+		-- perform raycast
+		local physicsResult = ( function () return {} end ) ()
+		for k, ent in pairs(physicsResult) do
+			local dist2 = ent.zombie.pos:dist2( self.zombie.pos )
+			if ent.entitytype == "zombie" then
+				if ent.teamid == self.globalmind.teamid then
+					if dist2 < 10000 then
+						-- influences friend separation
+					end
+					if dist2 < 50000 then
+						-- influences alineation
+						-- influences cohesion
+					end
+				else
+					if dist2 < 50000 then
+						-- influences strange zombie separation
+					end
+				end
+			elseif ent.entitytype == "wall" then
+				if dist2 < 50000 then
+					-- influences wall separation
+				end
+			end
+		end
 	end
-
 }
